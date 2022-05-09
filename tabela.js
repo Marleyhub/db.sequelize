e5526363e104ef95542f0095c0f997f7ed2cce5c
Gabriel Marley <cardoso_eng@outlook.com>
2022-05-09 18:59:05 -0300

    initial commit for the database all in one page first try

--- tabela.js
+++ tabela.js
@@ -1,18 +1,46 @@
-const Sequelize = require('sequelize')
-const database = require('./db')
+const Sequelize = require ('sequelize')
+const sequelize = new Sequelize ('sequelize', 'root', '20021976',{
+    host: 'localhost',
+    dialect:'mysql'   // mesmo usando o mariadb como servidor é possivel usar o dialeto do mysql com o driver mysql2
+})
 
-const Tabela = database.define('tabela',{
+const Moeda = sequelize.define('moedas',{
     exchange:{
-        type: Sequelize.STRING 
+       type: Sequelize.STRING 
     },
     crypto:{
         type: Sequelize.STRING
     },
-    cryptoUNT:{
-        type: Sequelize.FLOAT
+    cryptoUND:{
+        type: Sequelize.INTEGER
     },
-    USDT:{
+    USDT: {
         type: Sequelize.FLOAT
     }
-}) 
-module.exports = Tabela
+
+})
+Moeda.create({
+    exchange: "Binance",
+    crypto:"BTC",
+    cryptoUND:"1",
+    USDT:"31399"
+})*
+Moeda.create({
+    exchange: "Binance",
+    crypto:"Cardano",
+    cryptoUND:"1",
+    USDT:"3139970"
+})
+Moeda.create({
+    exchange: "Coinbase",
+    crypto:"ETH",
+    cryptoUND:"1",
+    USDT:"267845"
+})
+Moeda.create({
+    exchange: "Kraken",
+    crypto:"SOL",
+    cryptoUND:"1",
+    USDT:"7631"
+})
+//Moeda.sync({force:true})
